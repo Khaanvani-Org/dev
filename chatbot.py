634a1ba55520ed257220e3402e803661dbf7d9df
priyankasharma2410 <141417152+priyankasharma2410@users.noreply.github.com>
2023-11-06 12:54:46 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,15 +1,3 @@
-from langchain.chat_models import ChatOpenAI
-from langchain.chains import ConversationChain
-from langchain.chains.conversation.memory import ConversationBufferWindowMemory
-from langchain.prompts import (
-    SystemMessagePromptTemplate,
-    HumanMessagePromptTemplate,
-    ChatPromptTemplate,
-    MessagesPlaceholder
-)
-import streamlit as st
-from streamlit_chat import message
-from utils import *
 
 st.header("KhaanVaani")
 
