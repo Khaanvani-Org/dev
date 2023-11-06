a0fb98205cb26e16c37ac9c72964d11a695ffd8e
priyankasharma2410 <141417152+priyankasharma2410@users.noreply.github.com>
2023-11-06 13:13:48 +0530

    Update pytest.py

--- pytest.py
+++ pytest.py
@@ -1,14 +1,3 @@
-import os
-import sys
-
-# Get the current directory of pytest.py
-current_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Add the 'code' directory to the Python path
-code_dir = os.path.join(current_dir, 'code')
-sys.path.insert(0, code_dir)
-
-# Now you can import the chatbot module from the code directory
 from chatbot import chatbot
 
 import pytest
