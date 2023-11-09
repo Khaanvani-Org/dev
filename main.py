70fd19329a832f81905e792bbb588063cc50289a
priyankasharma2410 <141417152+priyankasharma2410@users.noreply.github.com>
2023-11-09 21:52:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 import random
+import unittest
 
+def generate_random_numbers(size):
+    return [random.randint(1, 100) for _ in range(size)]
 
-def calculate_square(number):
-    result = number * number
-    thread_name = threading.current_thread().name
-    print(f"Square of {number} is {result} (Thread: {thread_name})")
-    time.sleep(2)
+def calculate_average(numbers):
+    return sum(numbers) / len(numbers) if numbers else 0
 
-def main():
-    numbers = [1, 2, 3, 4, 5]
+def classify_numbers(numbers, average):
+    return ["above average" if num > average else "below average" for num in numbers]
 
-    threads = []
+class TestRandomCode(unittest.TestCase):
+    def test_generate_random_numbers(self):
+        result = generate_random_numbers(5)
+        self.assertEqual(len(result), 5)
 
-    for number in numbers:
-        thread = threading.Thread(target=calculate_square, args=(number,))
-        threads.append(thread)
-        thread.start()
+    def test_calculate_average(self):
+        average = calculate_average([1, 2, 3, 4, 5])
+        self.assertEqual(average, 3)
 
-    # Wait for all threads to complete
-    for thread in threads:
-        thread.join()
+    def test_classify_numbers(self):
+        numbers = [1, 2, 3, 4, 5]
+        average = calculate_average(numbers)
+        classification = classify_numbers(numbers, average)
+        self.assertEqual(classification, ["below average", "below average", "average", "above average", "above average"])
 
-    print("All threads have finished.")
+if __name__ == "__main__":
+    # Let's run the tests
+    unittest.main()
