5abbd22f58415b5b3782c4db2000c28acb3477df
Sunidhi ks <141416857+eng21ct0040@users.noreply.github.com>
2023-11-09 18:49:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-class Chatbot:
+
     def respond(self, user_input):
         if "hello" in user_input.lower():
             return "Hi there!"
@@ -6,10 +6,3 @@ class Chatbot:
             return "I don't understand."
 
 # Example usage
-chatbot = Chatbot()
-
-user_input = input("You: ")
-while user_input.lower() != "exit":
-    response = chatbot.respond(user_input)
-    print("Chatbot:", response)
-    user_input = input("You: ")
