f797ff5696f821db7b5a30e052e403b139b79765
priyankasharma2410 <141417152+priyankasharma2410@users.noreply.github.com>
2023-11-09 21:36:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import random
 
 class Chatbot:
+    def __init__(self):
+        self.goodbye_commands = ["exit", "bye", "quit"]
+
     def respond(self, user_input):
         user_input_lower = user_input.lower()
 
@@ -8,7 +11,7 @@ class Chatbot:
             return "Hi there!"
         elif "how are you" in user_input_lower:
             return "I'm doing well, thank you. How about you?"
-        elif "goodbye" in user_input_lower:
+        elif any(command in user_input_lower for command in self.goodbye_commands):
             return "Goodbye! Have a great day!"
         elif "joke" in user_input_lower:
             return self.tell_joke()
@@ -28,10 +31,25 @@ chatbot = Chatbot()
 
 # Conversation loop
 while True:
-    #user_input = input("You: ")
+    user_input = input("You: ")
 
-   # if user_input.lower() == "exit":
+    # Check if the user wants to exit
+    if any(command in user_input.lower() for command in chatbot.goodbye_commands):
         print("Chatbot: Goodbye!")
         break
 
- 
+    # Get the chatbot's response
+    response = chatbot.respond(user_input)
+    
+    # Print the chatbot's response
+    print("Chatbot:", response)
+
+    # Handle jokes separately for more interactive conversations
+    if "joke" in user_input.lower():
+        response = chatbot.tell_joke()
+        print("Chatbot:", response)
+
+    # Add more specific response handling based on user input if needed
+    # ...
+
+    # Continue the conversation loop
