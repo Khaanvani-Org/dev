983ea1ce0a3552503ab8dae8508987bd1baf534d
priyankasharma2410 <141417152+priyankasharma2410@users.noreply.github.com>
2023-11-09 21:04:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
-class SimpleChatbot:
-    def respond(self, user_input):
-        if "hello" in user_input.lower():
-            return "Hi there! How can I help you?"
-        else:
-            return "I'm a simple chatbot. I may not understand everything. Ask me anything!"
+import openai
 
+class GeneralChatbot:
+    def __init__(self, api_key):
+        openai.api_key = api_key
+
+    def respond(self, user_input):
+        prompt = f"User: {user_input}\nChatbot:"
+        response = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt=prompt,
+            max_tokens=150
+        )
+        return response.choices[0].text.strip()
+
+# Replace "YOUR_API_KEY" with your actual OpenAI API key
+api_key = "YOUR_API_KEY"
+chatbot = GeneralChatbot(api_key)
+
+# Conversation loop
+while True:
+    user_input = input("You: ")
+
+    if user_input.lower() == "exit":
+        print("Chatbot: Goodbye!")
+        break
+
+    response = chatbot.respond(user_input)
+    print("Chatbot:", response)
