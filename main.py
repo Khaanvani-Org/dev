4b66cc08eda883d481a41761bdd709af28a3f86f
Sunidhi ks <141416857+eng21ct0040@users.noreply.github.com>
2023-11-09 19:05:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
+
 class Chatbot:
     def respond(self, user_input):
-        if "hello" in user_input.lower():
+        user_input_lower = user_input.lower()
+
+        if "hello" in user_input_lower:
             return "Hi there!"
+        elif "how are you" in user_input_lower:
+            return "I'm doing well, thank you. How about you?"
+        elif "goodbye" in user_input_lower:
+            return "Goodbye! Have a great day!"
+        elif "joke" in user_input_lower:
+            return self.tell_joke()
         else:
-            return "I don't understand."
+            return "I don't understand. Can you ask me something else?"
+
+    def tell_joke(self):
+        jokes = [
+            "Why did the scarecrow win an award? Because he was outstanding in his field!",
+            "What do you call fake spaghetti? An impasta!",
+            "Why don't scientists trust atoms? Because they make up everything!"
+        ]
+        return random.choice(jokes)
+
+# Instantiate the chatbot
+chatbot = Chatbot()
+
+# Conversation loop
+while True:
+    user_input = input("You: ")
+
+    if user_input.lower() == "exit":
+        print("Chatbot: Goodbye!")
+        break
+
+    response = chatbot.respond(user_input)
+    print("Chatbot:", response)
