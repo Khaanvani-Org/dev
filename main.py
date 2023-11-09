7d327eb825215884e2c77540079fd7a51a592b42
Sunidhi ks <141416857+eng21ct0040@users.noreply.github.com>
2023-11-09 18:52:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-
+class Chatbot:
     def respond(self, user_input):
         if "hello" in user_input.lower():
             return "Hi there!"
         else:
             return "I don't understand."
-
-# Example usage
